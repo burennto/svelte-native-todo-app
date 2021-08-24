c927bf1ba39cd4ff8b654a2dcd8c47cc52d64bca
Brent Chuang <burennto@gmail.com>
2021-08-25 00:19:34 +1000

    Basic Design

--- app/App.svelte
+++ app/App.svelte
@@ -1,24 +1,17 @@
 <page>
-    <actionBar title="Svelte Native App" />
-    <gridLayout>
-        <label class="info" horizontalAlignment="center" verticalAlignment="middle" textWrap="true">
-            <formattedString>
-                <span class="fas" text="&#xf135;" />
-                <span text=" {message}" />
-            </formattedString>
-        </label>
-    </gridLayout>
+  <actionBar title="My Tasks" />
+
+  <tabs tabsPosition="bottom">
+    <tabStrip>
+      <tabStripItem title="To Do" />
+      <tabStripItem title="Completed" />
+    </tabStrip>
+
+    <tabContentItem>
+      <label textWrap="true">This tab will list active tasks and will let users add new tasks.</label>
+    </tabContentItem>
+    <tabContentItem>
+      <label textWrap="true">This tab will list compelted tasks for tracking.</label>
+    </tabContentItem>
+  </tabs>
 </page>
-
-<script lang="typescript">
-    let message: string = "Blank Svelte Native App"
-</script>
-
-<style>
-    .info .fas {
-        color: #3A53FF;
-    }
-    .info {
-        font-size: 20;
-    }
-</style>
